2bcf40df2edfd65bbaa18a570d64b5f0ab4cf2c9
raspberry <freebit4433@sina.cn>
2016-01-15 04:23:20 +0000

    init commit

---
+++ Application/Home/Controller/LoginController.class.php
@@ -0,0 +1,50 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+use Think\Model;
+use Org\Util\Vcode;
+class LoginController extends Controller{
+	public function index(){
+		$this->display();
+	}
+
+	public function submit(){
+		if (!IS_POST) {
+			$this->error('页面不存在！','index',1);
+		}
+		$verify = session('vcode');
+		$vcode = I('post.vcode');
+		if ($vcode !=$verify) {
+			$this->error('验证码输入有误！','index',1);
+		}
+		$userTable = D('Login');
+		if ($condition = $userTable->field('username,password')->create($_POST,Model::MODEL_INSERT)) {
+			//创建数据成功
+			if($result = $userTable->where($condition)->select()){
+				//查询成功，用户登陆成功
+				//写入session
+				session('username',$result[0]['username']);
+				session('isLogin',true);
+				$this->redirect('Index/index');
+			}else{
+				$this->error('用户名或密码不正确！','index',1);
+			}
+		}else{
+			//创建失败
+			$this->error($userTable->getError(),'index',1);
+		}
+	}
+
+	public function registerClick(){
+		$this->redirect('Register/index');
+	}
+
+	public function vcode(){
+		$vCode = new Vcode(60,20,4,1);
+		$vCodeString = $vCode->achieveString();
+		session('vcode',$vCodeString);
+		$vCode->achieveImage();
+		$vCode = NULL;
+	}
+}
+?>
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,40 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TP Comment</title>
+	<link rel="stylesheet" type="text/css" href="/Public/Home/css/index.css" />
+	<script type="text/javascript" src="/Public/Home/js/jQuery-v1.11.3.js"></script>
+	<script type="text/javascript" src="/Public/Home/js/json2.js"></script>
+	<script type="text/javascript" src="/Public/Home/js/index.js"></script>
+</head>
+<body>
+	<div id="wrapper">
+		<div id="comment">
+			<hr />
+			<p>你好，<span id="username"><?php echo ($username); ?></span><span id="loginUserName"></span><a id="logout" href="<?php echo U('Index/logout');?>">退出登陆</a></p>
+			<p>最近留言</p>
+			<form method='POST' action="<?php echo U('Index/submit');?>">
+				<textarea id="commentContent" name="comment" wrap="soft" maxlength=100></textarea>
+				<br />
+				<a id="submit">提 交</a>
+				<span id="submitURL" style="display:none;"><?php echo U('Index/submit');?></span>
+				<span id="refreshURL" style="display:none;"><?php echo U('Index/refresh');?></span>
+				<span id="deleteURL" style="display:none;"><?php echo U('Index/delete');?></span>
+			</form>
+		</div>
+		<div id="content">
+			<!-- <div id="c1" class="cX">
+				<img class="img" id="c1Img" src="/Public/Home/img/1.jpg" />
+				<a class="userName" id="c1UserName" href="">米卡</a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<span class="date" id="c1Date">2015-11-16 21:22:29</span>
+				<p clasa="text" id="c1Text">这是评论</p>
+				<a class="delete" id="c1Delete" href="">删除</a>
+			</div> -->
+		</div>
+		<div id="pageData">
+			
+		</div>
+	</div>
+</body>
+</html>
